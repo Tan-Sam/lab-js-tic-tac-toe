dd4f2b876810ebb3440768b8ec2548f57c8abf0d
sam <Tan-Sam@users.noreply.github.com>
2017-05-28 05:13:14 +0800

    resolved all bugs, refactored scan method into single method. archived unused functions.

--- debuggedScript.js
+++ debuggedScript.js
@@ -1,5 +1,43 @@
-function areTurnsCompleted(){
-  return $('td').filter((ind, elem, arr)=>{
-    return $(elem).text() === '';
-  }).length === 0;
+//  mark player cells
+function markCell(event) {
+
+  var targetCell = $(event.target);
+
+  if (isX_Turn) {
+    targetCell.text(xChar).css(xStyle);
+    whichPlayers_turnLabel.text(oPlayersTurn).delay(500);
+  }
+  else {
+    targetCell.text(oChar).css(oStyle);
+    whichPlayers_turnLabel.text(xPlayersTurn).delay(500);
+  }
+
+  isX_Turn = !isX_Turn;
+}
+
+function turnsExhausted() {
+  return noOfturnsLeft() === 0;
+}
+
+function noOfturnsLeft() {
+  return calculateTurns('');
+}
+
+function calculateTurns(char) {
+  return $('td').filter((i, e) => {
+    return e.textContent === char
+  }).length;
+}
+
+function xTurnsTaken() {
+  return calculateTurns('X');
+}
+
+function oTurnsTaken() {
+  return calculateTurns('O');
+}
+
+function threeTurnsReached() {
+  return xTurnsTaken() >= 3 ||
+    oTurnsTaken() >= 3;
 }
--- script.js
+++ script.js
@@ -1,169 +1,64 @@
-var isXsTurn = true;
-
-function calculateTurns(char){
-  return $('td').filter((i,e)=>{ return e.textContent === char}).length;
-}
-
-function xTurns(){
-  return calculateTurns('X');
-}
-
-function oTurns(){
-  return calculateTurns('O');
-}
-
-function noOfturnsLeft(){
-  return calculateTurns('');
-}
-
-function threeTurnsReached(){
-  return xTurns() === 3 ||
-         oTurns() === 3;
-}
+var isX_Turn = true;
 
 var winnerChar = null;
-// $('td').eq(4).data().num
-function horizontalScan(){
-  var winFound = false;
-  for (var row = 0; row < 3; row++) {
-    var column = row * 3;
 
-    var cell_1_char = $('td').eq(column).text();
-    var cell_2_char = $('td').eq(column+1).text();
-    var cell_3_char = $('td').eq(column+2).text();
-
-    if (cell_1_char === '') {
-      continue;   //  don't bother matching. 1st cell is empty.
-    }
-
-    winFound = (cell_1_char === cell_2_char) &&
-               (cell_2_char === cell_3_char);
-
-     if (winFound) {
-       winnerChar = cell_1_char;
-       return true;
-     }
-  }
-  return winFound;
-}
-
-function verticalScan() {
-  var winFound = false;
-  for (var i = 0; i < 3; i++) {
-
-    var cell_1_char = $('td').eq(i).text();
-    var cell_2_char = $('td').eq(i+3).text();
-    var cell_3_char = $('td').eq(i+6).text();
-
-    if (cell_1_char === '') {
-      continue; // prevent matching of 3 empty ''s.
-    }
-
-    winFound = (cell_1_char === cell_2_char) &&
-               (cell_2_char === cell_3_char);
-
-     if (winFound) {
-       winnerChar = cell_1_char;
-       return true;   //  break for loop
-     }
-  }
-  return winFound;
-}
-
-const diagonalArrays = [
-  [0,4,8],
-  [2,4,6],
-];
-
-function diagonalScan() {
-  var winFound = false;
-  diagonalArrays.forEach((diagArrIndex)=>{
-
-    var cell_1_char = $('td').eq(diagArrIndex[0]).text();
-    var cell_2_char = $('td').eq(diagArrIndex[1]).text();
-    var cell_3_char = $('td').eq(diagArrIndex[2]).text();
-
-    if (cell_1_char !== '') { // prevent matching of 3 '' cells.
-
-      winFound = (cell_1_char === cell_2_char) &&
-                 (cell_2_char === cell_3_char);
-
-      if (winFound) {
-        winnerChar = cell_1_char;
-        return false; // breakOut of forEach.
-      }
-    }
-  });
-  return winFound;
-}
-
-function isCellFilled(e){
+function isCellFilled(e) {
   return (e.target.textContent !== '');
 }
 
-function tickClicked(event){
-  if (isCellFilled(event) ||  // ignore marked cells.
-      hasPlayerWon()) {       // ignore when game ends.
-      return;
+function tickClicked(event) {
+  if (isCellFilled(event) || // ignore marked cells.
+      playerWon()) { // ignore when game ends.
+    return;
   }
   markCell(event);
 
-  setTimeout(()=>{
-    if (threeTurnsReached()) {
-      if (hasPlayerWon()) {
-        alert('Player ' + winnerChar + " has won!");
-      }
-      else if (areTurnsCompleted()) {
-        alert('Turns exhausted. Reset game.');
-      }
+  if (threeTurnsReached()) {
+    if (playerWon()) {
+      alert('Player ' + winnerChar + " has won!");
+    } else if (turnsExhausted()) {
+      alert('Turns exhausted. Reset game.');
     }
-  }, 200);
+  }
 }
 
-const xStyle = {color:'red'};
-const oStyle = {color:'green', 'font-weight':'bold',};
+const xStyle = {
+  color: 'red'
+};
+const oStyle = {
+  color: 'green',
+  'font-weight': 'bold',
+};
 
-const xPlayersTurn = 'It is X\'s turn';
-const oPlayersTurn = 'It is O\'s turn';
+const alertAnimation = {};
+
+const xPlayersTurn = 'X\'s turn';
+const oPlayersTurn = 'O\'s turn';
+const playerWonTemplate = 'Player {{who}} has won. \nReset the game to play again.';
 
 const xChar = 'X';
 const oChar = 'O';
 var whichPlayers_turnLabel = $('.playerTurn');
 
-//  mark player cells
-function markCell(event){
+function playerWon() {
+  var result = scan('horizontal')||
+               scan('vertical')||
+               scan('diagonal');
 
-  var targetCell = $(event.target);
-
-  if (isXsTurn) {
-    targetCell.text(xChar).css(xStyle);
-    whichPlayers_turnLabel.text(oPlayersTurn);
+  if (result && whichPlayers_turnLabel.text().includes("turn")) {
+    whichPlayers_turnLabel.text(playerWonTemplate.replace(/{{who}}/, winnerChar));
   }
-  else {
-    targetCell.text(oChar).css(oStyle);
-    whichPlayers_turnLabel.text(xPlayersTurn);
-  }
-
-  isXsTurn = !isXsTurn;
-}
-
-function hasPlayerWon(){
-  return horizontalScan() ||
-         verticalScan() ||
-         diagonalScan();
+  return result;
 }
 
 function resetClicked(e) {
   $('td').text('');
-  isXsTurn = true;
-  whichPlayers_turnLabel.text(xPlayersTurn);
+  // isX_Turn = true;
+  var nxPlayer = isX_Turn? xPlayersTurn : oPlayersTurn;
+  whichPlayers_turnLabel.text(nxPlayer);
 }
 
-$(document).ready(()=>{
+$(document).ready(() => {
   $('td').click(tickClicked);
   $('#reset').click(resetClicked);
 });
-
-function functionTester(){
-
-}
